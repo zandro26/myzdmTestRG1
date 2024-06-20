8b3c828ecec60b2b9d1f04280e27f9f21c2a2fb8
zandro26 <zansm33@gmail.com>
2024-06-20 14:19:38 +1200

    newest update now

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create a Resource Group if it doesn’t exist
 resource "azurerm_resource_group" "tfexample" {
-  name     = "my-terraform-rg"
+  name     = "mytstzdmterraformrg"
   location = "East US"
 }
 
--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 # Input variable: Name of Storage Account
 variable "storage_account_name" {
   description = "The name of the storage account. Must be globally unique, length between 3 and 24 characters and contain numbers and lowercase letters only."
-  default     = "mytfstorageaccount"
+  default     = "myzdmtsttfstorageaccount"
 }
 
 # Input variable: Name of Storage container
